a91183daf0a16a7a084e75f6a4ba02ec4258c73e
KENNETH PHANG <bunnyppl@hotmail.com>
2022-07-25 16:07:59 +0800

    upd

--- src/main/java/sg/edu/nus/iss/Workshop14/service/ContactsRedis.java
+++ src/main/java/sg/edu/nus/iss/Workshop14/service/ContactsRedis.java
@@ -1,5 +1,29 @@
 package sg.edu.nus.iss.Workshop14.service;
 
-public class ContactsRedis {
-    
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import sg.edu.nus.iss.Workshop14.model.Contact;
+
+@Service
+public class ContactsRedis implements ContactsRepo {
+    private static final Logger logger = LoggerFactory.getLogger(ContactsRedis.class);
+
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
+    @Override
+    public void save(final Contact ctc) {
+        redisTemplate.opsForValue().set(ctc.getId(), ctc);
+    }
+
+    @Override
+    public Contact findById(final String contactId) {
+        Contact result = (Contact) redisTemplate.opsForValue().get(contactId);
+        logger.info(">>> " + result.getEmail());
+        return result;
+    }
 }
